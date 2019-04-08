c652a02fa4d3314dd2524de0eb96444e053a6e11
Anis Noorudheen <35500897+anisnoorudheen@users.noreply.github.com>
2019-04-08 12:29:23 +0530

    Update main_version.mk

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,6 +1,6 @@
 # Version of the ROM
-REVENGEOS_CODENAME := Pasta
-REVENGEOS_REVISION := 2.3
+aosep_CODENAME := stable
+aosep_REVISION := 2.0
 CAF_REVISION := LA.UM.7.3.r1-06900-sdm845.0
 
 ifndef REVENGEOS_BUILDTYPE
@@ -10,12 +10,12 @@ endif
 TARGET_PRODUCT_SHORT := $(TARGET_PRODUCT)
 TARGET_PRODUCT_SHORT := $(subst revengeos_,,$(TARGET_PRODUCT_SHORT))
 
-REVENGEOS_VERSION := $(REVENGEOS_REVISION)-$(REVENGEOS_CODENAME)-$(REVENGEOS_BUILDTYPE)-$(TARGET_PRODUCT_SHORT)-$(shell date -u +%Y%m%d-%H%M)
+REVENGEOS_VERSION := $(aosep_REVISION)-$(REVENGEOS_CODENAME)-$(aosep_BUILDTYPE)-$(TARGET_PRODUCT_SHORT)-$(shell date -u +%Y%m%d-%H%M)
 
 ADDITIONAL_BUILD_PROPERTIES += BUILD_DISPLAY_ID="$(BUILD_ID)-$(shell whoami)@$(shell hostname)"
 
 # Apply it to build.prop
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.caf.revision=$(CAF_REVISION) \
-    ro.revengeos.version=RevengeOS-$(REVENGEOS_VERSION) \
-    ro.ota.revengeos.version=$(REVENGEOS_VERSION)
+    #ro.caf.revision=$(CAF_REVISION) \
+    #ro.revengeos.version=RevengeOS-$(REVENGEOS_VERSION) \
+    #ro.ota.revengeos.version=$(REVENGEOS_VERSION)
